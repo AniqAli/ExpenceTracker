abaea688648435471ee76ef22065b27919074e6a
AniqAli <aniq@tecizeverything.com>
2022-05-12 00:00:28 +0500

    add, fetch data done with localStorage

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,30 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const AddTransaction = () => {
-  const [trans, setTrans] = useState({
-    transDesc: "",
-    transAmount: "",
-  });
+  const [transDesc, setTransDesc] = useState();
+  const [transAmount, setTransAmount] = useState();
+  const [trans, setTrans] = useState([]);
 
-  const handleChange = (e) => {
-    console.log("handleChange->", e.target.value);
-    // const val = e.target.value;
-    // setTrans({
-    //   ...trans,
-    //   [e.target.value]: val,
-    // });
-    setTrans({
-      transDesc: e.target.value,
-      transAmount: e.target.value,
-    });
-
-    // setTransDesc(e.target.value);
-  };
-
-  const handleBtn = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
+    let storedData = JSON.parse(localStorage.getItem("transaction")) || [];
+    console.log("stored data", storedData);
 
-    // console.log({ transDesc });
+    const body = { transDesc, transAmount };
+    console.log("new data", body);
+    const payload = storedData.concat(body);
+
+    console.log({ payload });
+    localStorage.setItem("transaction", JSON.stringify(payload));
+
+    // var storedData = JSON.parse(localStorage.getItem("transaction"));
   };
 
   return (
@@ -32,54 +25,59 @@ const AddTransaction = () => {
       <div style={{ padding: "1rem" }}>
         <h2>Add Transaction</h2>
         <hr />
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            padding: "0 1rem",
-          }}
-        >
-          <input
-            type="text"
-            name="transDesc"
-            value={trans.transDesc}
-            onChange={handleChange}
-            autoComplete="off"
-            placeholder="Description"
+        <form onSubmit={handleSubmit}>
+          <div
             style={{
-              padding: "12px",
-              borderRadius: "3px",
-              fontSize: "14px",
+              display: "flex",
+              justifyContent: "space-between",
+              padding: "0 1rem",
             }}
-          />
-          <input
-            type="number"
-            name="transAmount"
-            value={trans.transAmount}
-            onChange={handleChange}
-            autoComplete="off"
-            placeholder="Amount"
+          >
+            <input
+              type="text"
+              name="transDesc"
+              value={transDesc}
+              onChange={(e) => setTransDesc(e.target.value)}
+              //   onChangeText={setTransDesc}
+              //   onChange={handleChange}
+              autoComplete="off"
+              placeholder="Description"
+              style={{
+                padding: "12px",
+                borderRadius: "3px",
+                fontSize: "14px",
+              }}
+            />
+            <input
+              type="number"
+              name="transAmount"
+              value={transAmount}
+              onChange={(e) => setTransAmount(e.target.value)}
+              //   onChangeText={setTransAmount}
+              //   onChange={handleChange}
+              autoComplete="off"
+              placeholder="Amount"
+              style={{
+                padding: "12px",
+                borderRadius: "3px",
+                fontSize: "14px",
+              }}
+            />
+          </div>
+          <button
             style={{
-              padding: "12px",
+              backgroundColor: "darkslategrey",
+              color: "whitesmoke",
+              padding: "0.8rem 0.8rem",
+              marginTop: "1rem",
+              border: "none",
               borderRadius: "3px",
-              fontSize: "14px",
+              cursor: "pointer",
             }}
-          />
-        </div>
-        <button
-          style={{
-            backgroundColor: "darkslategrey",
-            color: "whitesmoke",
-            padding: "0.8rem 0.8rem",
-            marginTop: "1rem",
-            border: "none",
-            borderRadius: "3px",
-            cursor: "pointer",
-          }}
-          onClick={handleBtn}
-        >
-          Add Transaction
-        </button>
+          >
+            Add Transaction
+          </button>
+        </form>
       </div>
     </>
   );
--- src/components/TransactionHistory.jsx
+++ src/components/TransactionHistory.jsx
@@ -1,34 +1,99 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const TransactionHistory = () => {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("transaction"));
+    console.log({ data });
+    setTransactions(data);
+  }, []);
+
   return (
     <>
       <div style={{ padding: "2rem" }}>
         <h2>Transaction History</h2>
         <hr />
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            backgroundColor: "lightgray",
-            padding: "0 1rem",
-          }}
-        >
-          <p>asd</p>
-          <p>$2</p>
-          <button
-            style={{
-              backgroundColor: "red",
-              color: "whitesmoke",
-              margin: "10px 1px",
-              border: "none",
-              borderRadius: "3px",
-              cursor: "pointer",
-            }}
-          >
-            Delete
-          </button>
-        </div>
+        {transactions.map((trans, i) => {
+          console.log({ trans });
+          return (
+            // <div
+            //   key={i}
+            //   style={{
+            //     display: "flex",
+            //     justifyContent: "space-between",
+            //     backgroundColor: "lightgray",
+            //     padding: "0 1rem",
+            //   }}
+            // >
+            //   <p>{trans.transDesc}</p>
+            //   <p>{trans.transAmount}</p>
+            //   <button
+            //     style={{
+            //       backgroundColor: "red",
+            //       color: "whitesmoke",
+            //       margin: "10px 1px",
+            //       border: "none",
+            //       borderRadius: "3px",
+            //       cursor: "pointer",
+            //     }}
+            //   >
+            //     Delete
+            //   </button>
+            // </div>
+            <table style={{ width: "100%" }}>
+              <thead>
+                <tr>
+                  <th>Description</th>
+                  <th>Amount</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr key={i}>
+                  <td>{trans.transDesc}</td>
+                  <td>{trans.transAmount}</td>
+                  <td>
+                    <button
+                      style={{
+                        backgroundColor: "red",
+                        color: "whitesmoke",
+                        margin: "10px 1px",
+                        border: "none",
+                        borderRadius: "3px",
+                        cursor: "pointer",
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          );
+          //   <div
+          //     style={{
+          //       display: "flex",
+          //       justifyContent: "space-between",
+          //       backgroundColor: "lightgray",
+          //       padding: "0 1rem",
+          //     }}
+          //   >
+          //     <p>{trans.transDesc}</p>
+          //     <button
+          //       style={{
+          //         backgroundColor: "red",
+          //         color: "whitesmoke",
+          //         margin: "10px 1px",
+          //         border: "none",
+          //         borderRadius: "3px",
+          //         cursor: "pointer",
+          //       }}
+          //     >
+          //       Delete
+          //     </button>
+          //   </div>;
+        })}
       </div>
     </>
   );
